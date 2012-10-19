6389458669c8852ef8ad9742025cae867abebd3e
Anthony DiSanti <anthony.disanti@gmail.com>
2012-10-18 23:43:54 -0400

    Modified the git update-submodules alias.

--- settings/git.bash
+++ settings/git.bash
@@ -4,4 +4,4 @@ git config --global color.ui true
 git config --global help.autocorrect 1
 git config --global core.excludesfile ~/.gitignore_global
 git config --global alias.showgraph "log --graph --abbrev-commit --pretty=oneline"
-git config --global alias.update-submodules "!echo 'Checking out master...'; git submodule foreach git checkout master; echo '\nPulling...'; git submodule foreach git pull"
+git config --global alias.update-submodules "!echo 'Initing...'; git submodule init; echo '\nUpdating...'; git submodule update; echo '\nChecking out master...'; git submodule foreach git checkout master; echo '\nPulling...'; git submodule foreach git pull"
