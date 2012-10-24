c2e22ed98659c5615d883cbde9114099b116cae4
Anthony DiSanti <anthony.disanti@gmail.com>
2012-10-24 10:23:52 -0400

    Continued working on git colors.

--- settings/git.bash
+++ settings/git.bash
@@ -13,16 +13,28 @@ GIT_COLOR_RESET='%C(reset)'
 declare -a GIT_COLORS=('normal' 'black' 'red' 'green' 'yellow' 'blue' 'magenta' 'cyan' 'white')
 declare -a GIT_COLOR_MODIFIERS=('bold' 'dim' 'ul' 'blink' 'reverse')
 GIT_COLOR_TEST_FORMAT=''
+MAX_COLOR_LENGTH=0
 
 for GIT_COLOR in ${GIT_COLORS[@]}; do
+  if [[ ${#GIT_COLOR} > $MAX_COLOR_LENGTH ]]; then
+    MAX_COLOR_LENGTH=${#GIT_COLOR}
+  fi
+
   GIT_COLOR_TEST_FORMAT="$GIT_COLOR_TEST_FORMAT%n%C($GIT_COLOR)$GIT_COLOR$GIT_COLOR_RESET"
   for GIT_COLOR_MODIFIER in ${GIT_COLOR_MODIFIERS[@]}; do
     GIT_COLOR_TEST_FORMAT="$GIT_COLOR_TEST_FORMAT|%C($GIT_COLOR $GIT_COLOR_MODIFIER)$GIT_COLOR $GIT_COLOR_MODIFIER$GIT_COLOR_RESET"
   done
 done
 
-AWK_FORMAT='%30s\n'
-AWK_SCRIPT="{ printf \"$AWK_FORMAT\", \$3 }"
+COLUMN_PADDING=3
+AWK_FORMAT="%${MAX_COLOR_LENGTH}s"
+for GIT_COLOR_MODIFIER in ${GIT_COLOR_MODIFIERS[@]}; do
+  AWK_FORMAT="$AWK_FORMAT%$(($MAX_COLOR_LENGTH + $COLUMN_PADDING + ${#GIT_COLOR_MODIFIER} + 1))s"
+done
+AWK_FORMAT=$AWK_FORMAT'\n'
+
+AWK_SCRIPT='{ printf "'$AWK_FORMAT'", $1, $2, $3, $4, $5, $6 }'
+echo $AWK_SCRIPT
 git config --global alias.colors "!git log -n1 --pretty=format:'$GIT_COLOR_TEST_FORMAT' | awk -F\| '$AWK_SCRIPT'"
 
 # UI Aliases
