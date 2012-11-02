824ff2000f5d1258fbd8b6eb388169c62f520318
Anthony DiSanti <anthony.disanti@gmail.com>
2012-11-02 06:52:08 -0400

    Got git colors fully working. Broke the git settings into multiple files.

--- settings/git.bash
+++ settings/git.bash
@@ -1,43 +1,20 @@
 #!/usr/bin/env bash
 
-# Settings
-git config --global color.ui true
-git config --global help.autocorrect 1
-git config --global core.excludesfile ~/.gitignore_global
-
-# Helper Aliases
-git config --global alias.update-submodules "!echo 'Initing...'; git submodule init; echo '\nUpdating...'; git submodule update; echo '\nChecking out master...'; git submodule foreach git checkout master; echo '\nPulling...'; git submodule foreach git pull"
-
-# git colors
-GIT_COLOR_RESET='%C(reset)'
-declare -a GIT_COLORS=('normal' 'black' 'red' 'green' 'yellow' 'blue' 'magenta' 'cyan' 'white')
-declare -a GIT_COLOR_MODIFIERS=('bold' 'dim' 'ul' 'blink' 'reverse')
-GIT_COLOR_TEST_FORMAT=''
-MAX_COLOR_LENGTH=0
-
-for GIT_COLOR in ${GIT_COLORS[@]}; do
-  if [[ ${#GIT_COLOR} > $MAX_COLOR_LENGTH ]]; then
-    MAX_COLOR_LENGTH=${#GIT_COLOR}
-  fi
-
-  GIT_COLOR_TEST_FORMAT="$GIT_COLOR_TEST_FORMAT%n%C($GIT_COLOR)$GIT_COLOR$GIT_COLOR_RESET"
-  for GIT_COLOR_MODIFIER in ${GIT_COLOR_MODIFIERS[@]}; do
-    GIT_COLOR_TEST_FORMAT="$GIT_COLOR_TEST_FORMAT|%C($GIT_COLOR $GIT_COLOR_MODIFIER)$GIT_COLOR $GIT_COLOR_MODIFIER$GIT_COLOR_RESET"
+# Finds the dereferenced directory of the current script
+get_SCRIPTDIR () {
+  local SOURCE="${BASH_SOURCE[0]}"
+  local DIR="$( dirname "$SOURCE" )"
+  while [ -h "$SOURCE" ]
+  do 
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
   done
-done
+  SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+}
 
-COLUMN_PADDING=3
-AWK_FORMAT="%${MAX_COLOR_LENGTH}s"
-for GIT_COLOR_MODIFIER in ${GIT_COLOR_MODIFIERS[@]}; do
-  AWK_FORMAT="$AWK_FORMAT%$(($MAX_COLOR_LENGTH + $COLUMN_PADDING + ${#GIT_COLOR_MODIFIER} + 1))s"
-done
-AWK_FORMAT=$AWK_FORMAT'\n'
+get_SCRIPTDIR
 
-#AWK_SCRIPT='{ printf "'$AWK_FORMAT'", $1, $2, $3, $4, $5, $6 }'
-AWK_SCRIPT="{ printf \"$AWK_FORMAT\""
-AWK_SCRIPT=$AWK_SCRIPT', $1, $2, $3, $4, $5, $6 }'
-echo $AWK_SCRIPT
-git config --global alias.colors "!git log -n1 --pretty=format:'$GIT_COLOR_TEST_FORMAT' | awk -F\| '$AWK_SCRIPT'"
-
-# UI Aliases
-git config --global alias.graph "log --graph --abbrev-commit --pretty=oneline"
+source $SCRIPTDIR/git/settings.bash
+source $SCRIPTDIR/git/aliases.bash
+source $SCRIPTDIR/git/colors.bash
