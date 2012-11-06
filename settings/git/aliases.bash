7890448bf710ab581586ee71d5b3c27deb431d4d
Anthony DiSanti <anthony.disanti@gmail.com>
2012-11-06 00:21:27 -0500

    Added additional aliases and settings taken from jukie's .gitconfig

--- settings/git/aliases.bash
+++ settings/git/aliases.bash
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 # Abbreviation Aliases
-git config --global alias.cam "commit -am"
+git config --global alias.cam 'commit -am'
+git config --global alias.subdo 'submodule foreach'
+git config --global alias.ff 'merge --ff-only'
+git config --global alias.staged 'diff --cached'
 
-# Helper Aliases
+# Helper Functions
 git config --global alias.update-submodules "!echo 'Initing...'; git submodule init; echo '\nUpdating...'; git submodule update; echo '\nChecking out master...'; git submodule foreach git checkout master; echo '\nPulling...'; git submodule foreach git pull"
 
 # UI Aliases
