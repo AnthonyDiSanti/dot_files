e06b544699444c0cbb195595454532e15f54a769
Anthony DiSanti <anthony@Anthonys-MacBook-Pro.local>
2018-01-02 03:38:56 -0500

    Moves All Home Directory Files to a Subdirectory
    * Moves all files that are symlinked from the user's home directory into a
      "home" subdirectory in the git repo
    * Updates the gitignore to reference the new directory structure
    * Updates site.pp to reference the new directory structure

--- site.pp
+++ site.pp
@@ -1,29 +1,29 @@
 file { "${home_dir}/.bash_profile":
   ensure => 'link',
-  target => "${dotfiles_dir}/.bash_profile",
+  target => "${dotfiles_dir}/home/.bash_profile",
 }
 
 file { "${home_dir}/.vim":
   ensure => 'link',
-  target => "${dotfiles_dir}/.vim",
+  target => "${dotfiles_dir}/home/.vim",
 }
 
 file { "${home_dir}/.tmux.conf":
   ensure => 'link',
-  target => "${dotfiles_dir}/.tmux.conf",
+  target => "${dotfiles_dir}/home/.tmux.conf",
 }
 
 file { "${home_dir}/.gitignore_global":
   ensure => 'link',
-  target => "${dotfiles_dir}/.gitignore_global",
+  target => "${dotfiles_dir}/home/.gitignore_global",
 }
 
 file { "${home_dir}/.vimpagerrc":
   ensure => 'link',
-  target => "${dotfiles_dir}/.vimpagerrc",
+  target => "${dotfiles_dir}/home/.vimpagerrc",
 }
 
 file { "${home_dir}/.vimrc":
   ensure => 'link',
-  target => "${dotfiles_dir}/.vimrc",
+  target => "${dotfiles_dir}/home/.vimrc",
 }
