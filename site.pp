2848a321c6fb7b0ec52eb3f5574777a3da314c5f
Anthony DiSanti <anthony@Anthonys-MacBook-Pro.local>
2018-01-02 03:54:03 -0500

    Adds the Backup Parameter to All Puppet Symlinks

--- site.pp
+++ site.pp
@@ -1,29 +1,35 @@
 file { "${home_dir}/.bash_profile":
   ensure => 'link',
   target => "${dotfiles_dir}/home/.bash_profile",
+  backup => true,
 }
 
 file { "${home_dir}/.vim":
   ensure => 'link',
   target => "${dotfiles_dir}/home/.vim",
+  backup => true,
 }
 
 file { "${home_dir}/.tmux.conf":
   ensure => 'link',
   target => "${dotfiles_dir}/home/.tmux.conf",
+  backup => true,
 }
 
 file { "${home_dir}/.gitignore_global":
   ensure => 'link',
   target => "${dotfiles_dir}/home/.gitignore_global",
+  backup => true,
 }
 
 file { "${home_dir}/.vimpagerrc":
   ensure => 'link',
   target => "${dotfiles_dir}/home/.vimpagerrc",
+  backup => true,
 }
 
 file { "${home_dir}/.vimrc":
   ensure => 'link',
   target => "${dotfiles_dir}/home/.vimrc",
+  backup => true,
 }
